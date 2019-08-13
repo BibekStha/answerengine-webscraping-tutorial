e4b1f3a0a18658b497f7c49aa6443a200f3af082
Bibek Shrestha <bibekmanshrestha@gmail.com>
2019-08-13 19:56:47 -0400

    Update seeder.rb

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -1,5 +1,5 @@
 pages << {
-	page_type: 'listings',
-	method: 'GET',
-	ulr: 'https://www.ebay.com/b/Apple-iPhone/9335/bn_319682'
+  page_type: 'listings',
+  method: 'GET',
+  ulr: 'https://www.ebay.com/b/Apple-iPhone/9335/bn_319682'
 }
